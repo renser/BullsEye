79e16090952a8b337f8fd9677371048a52d001a4
renser <zhuhuan@huanzhu.net>
2014-11-19 22:51:49 +0800

    Revert d434e72..a3d3d66
    This rolls back to commit d434e720e07336742eb6a5e122dfe8b44cd0881e.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -13,11 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        var lable = UILabel()
+        var lable1 = UILabel()
         
-
-
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
--- README.md
+++
@@ -1,9 +0,0 @@
-BullsEye
-========
-
-This is my first Project, a single game.
-<<<<<<< Updated upstream
-=======
-
-In this project there are some basic UI controllers such as lables, buttons, sliders. There is also a view transtion sample code. 
->>>>>>> Stashed changes
