a3d3d664d0d10f88643b10d31ce4b8312bd17232
renser <zhuhuan@huanzhu.net>
2014-11-19 22:51:20 +0800

    test2

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        var lable = UILabel()
+        
+
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
